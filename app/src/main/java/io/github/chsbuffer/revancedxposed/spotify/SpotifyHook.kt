2dda863a30b04b4b733060600249022037a02789
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-06 16:14:45 -0300

    Update SpotifyHook.kt

--- app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
@@ -31,7 +31,6 @@ class SpotifyHook(app: Application, lpparam: LoadPackageParam) : BaseHook(app, l
         ::Extension,
         ::SanitizeSharingLinks,
         ::UnlockPremium,
-        ::HideCreateButton,
         ::FixThirdPartyLaunchersWidgets
     )
 
